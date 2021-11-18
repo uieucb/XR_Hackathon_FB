a556aabecc855aef9873d899654077b02d91cb61
UIE <77365000+uieucb@users.noreply.github.com>
2021-11-18 16:29:50 -0400

    Signal filtering implemented

--- Assets/_bruno/myscripts/SignalFiltering.cs
+++ Assets/_bruno/myscripts/SignalFiltering.cs
@@ -11,13 +11,16 @@ using brainflow.math;
 
     public class SignalFiltering: MonoBehaviour
     {
-        public static double[,] unprocessed_data;
+        public static double[,] unprocessed_data = null;
         public static double[,] empty_data;
+
+        public static double [] filtered;
+        /*
         public static double[] filtered5;
         public static double[] filtered12;
         public static double[] filtered19;
         public static double[] filtered25;
-
+        */
         public static String enemySelected;
 
         //public GameObject textObject;
@@ -32,27 +35,28 @@ using brainflow.math;
 
         void Update ()
         {
-            //THIS FUNCTION WAS MAINLY TESTED WITH THE SYNTHETIC BOARD
-            int board_id = staticPorts.boardIdSelected;
-            //double[,] unprocessed_data = openbciConnection.data;
-//            Debug.Log("Num elements SF unprocessed data: " + unprocessed_data.GetLength(1));
-            int[] eeg_channels = staticPorts.eeg_channels;
-            
-            
-            if (unprocessed_data.GetLength(1) % (staticPorts.sampling_rate*2) == 0){
-                //print("Entering the segmentation loop");
-                //print((unprocessed_data.GetRow (eeg_channels[0]), staticPorts.sampling_rate, 15.0, 5.0, 2, (int)FilterTypes.BUTTERWORTH, 0.0));
-                print("ENTERED THE IF");
-                for (int i = 0; i < eeg_channels.Length; i++){
+            if(unprocessed_data!=null){
+                //THIS FUNCTION WAS MAINLY TESTED WITH THE SYNTHETIC BOARD
+                int board_id = staticPorts.boardIdSelected;
+                
+                int[] eeg_channels = staticPorts.eeg_channels;
+                
+                
+                if (unprocessed_data.GetLength(1) % (staticPorts.sampling_rate*2) == 0){
                     
-                    //FILTERING THE SIGNAL WITH 25,19,12,5
-                    filtered25 = DataFilter.perform_bandpass (unprocessed_data.GetRow (eeg_channels[i]), staticPorts.sampling_rate, 25.0, 5.0, 2, (int)FilterTypes.BUTTERWORTH, 0.0);
-                    filtered19 = DataFilter.perform_bandpass (unprocessed_data.GetRow (eeg_channels[i]), staticPorts.sampling_rate, 19.0, 5.0, 2, (int)FilterTypes.BUTTERWORTH, 0.0);
-                    filtered12 = DataFilter.perform_bandpass (unprocessed_data.GetRow (eeg_channels[i]), staticPorts.sampling_rate, 12.0, 5.0, 2, (int)FilterTypes.BUTTERWORTH, 0.0);
-                    filtered5 = DataFilter.perform_bandpass (unprocessed_data.GetRow (eeg_channels[i]), staticPorts.sampling_rate, 5.0, 5.0, 2, (int)FilterTypes.BUTTERWORTH, 0.0);
-
+                    print("ENTERED THE IF");
+                    for (int i = 0; i < eeg_channels.Length; i++){
+                        
+                        //FILTERING THE SIGNAL WITH NOTCH AND BANDPASS FILTER
+                        filtered = DataFilter.remove_environmental_noise(unprocessed_data.GetRow (eeg_channels[i]),staticPorts.sampling_rate,(int)NoiseTypes.FIFTY);
+                        filtered = DataFilter.perform_bandpass(unprocessed_data.GetRow(eeg_channels[i]),staticPorts.sampling_rate,10.0,3.0,2,(int)FilterTypes.BUTTERWORTH, 0.0);
+                        
+                    }
+                    print(filtered.GetLength(0));
                 }
+            
 
+                /*
                 double sum5 = 0,sum12 = 0, sum19 = 0, sum25 = 0;
                 for(int j = 0;j < filtered5.Length;j++){
                     sum5 = sum5 + filtered5[j];
@@ -92,7 +96,7 @@ using brainflow.math;
                     enSel.text  = Convert.ToString(-1);
                 }
                 //print("AVERAGE FILTERED "+sum /filtered.Length );
-
+                */
                 unprocessed_data = empty_data;
             }
             
